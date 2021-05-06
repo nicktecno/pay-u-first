f2d43aee511b594b4854f87c79d52de9e3905d34
nicktecno <nick.tecno@gmail.com>
2021-05-06 17:58:57 -0300

    feat: list users route

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import Koa from 'koa'
+import bodyParser from 'koa-bodyparser'
+
+import { router } from './routes'
+
+const app = new Koa()
+
+app.use(bodyParser())
+
+app.use(bodyParser())
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+app.listen(3000)
