0d38fb6e986e6f7ddeeaf483156a6c1f394b5276
nicktecno <nick.tecno@gmail.com>
2021-05-10 12:19:11 -0300

    feat: create user schema and migration

---
+++ prisma/migrations/20210510152627_initial/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deletedAt" TIMESTAMP(3),
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users.email_unique" ON "users"("email");
